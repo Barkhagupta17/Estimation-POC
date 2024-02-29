20f4eb70a0c1a4f9787a19389d8e3f662ef37168
Sandip Savani <sandip.savani@one10marketing.com>
2024-02-29 18:52:32 +0530

    Remove extra code

---
+++ src/menu-items/pages.js
@@ -0,0 +1,66 @@
+// assets
+import { IconKey } from '@tabler/icons-react';
+
+// constant
+const icons = {
+  IconKey
+};
+
+// ==============================|| EXTRA PAGES MENU ITEMS ||============================== //
+
+const pages = {
+  id: 'pages',
+  title: 'Projects',
+  caption: 'Projects Estimation',
+  type: 'group',
+  children: [
+    {
+      id: 'authentication',
+      title: 'Toyota',
+      type: 'collapse',
+      icon: icons.IconKey,
+
+      children: [
+        {
+          id: 'login3',
+          title: 'Estimation 1',
+          type: 'item',
+          url: '/pages/login/login3',
+          target: true
+        },
+        {
+          id: 'register3',
+          title: 'Estimation 2',
+          type: 'item',
+          url: '/pages/register/register3',
+          target: true
+        }
+      ]
+    },
+    {
+      id: 'envu',
+      title: 'Envu',
+      type: 'collapse',
+      icon: icons.IconKey,
+
+      children: [
+        {
+          id: 'login3',
+          title: 'Estimation 1',
+          type: 'item',
+          url: '/pages/login/login3',
+          target: true
+        },
+        {
+          id: 'register3',
+          title: 'Estimation 2',
+          type: 'item',
+          url: '/pages/register/register3',
+          target: true
+        }
+      ]
+    }
+  ]
+};
+
+export default pages;
